916d301e04728d14c9ce8709e85bffcc48345e4b
harukaaa-u <harukaaa.u@gmail.com>
2024-03-20 11:27:06 -0700

    meal detail model updated

--- src/main/java/eatpro/model/MealDetails.java
+++ src/main/java/eatpro/model/MealDetails.java
@@ -2,42 +2,46 @@ package eatpro.model;
 
 public class MealDetails {
     protected int mealDetailId;
-    protected int foodId;
-    protected int mealId;
-
-    public MealDetails(int mealDetailId, int foodId, int mealId) {
-        this.mealDetailId = mealDetailId;
-        this.foodId = foodId;
-        this.mealId = mealId;
-    }
-    
-    public MealDetails(int foodId, int mealId) {
-		this.foodId = foodId;
-		this.mealId = mealId;
+    protected Food food;
+    protected Meals meal;
+	
+    public MealDetails(int mealDetailId, Food food, Meals meal) {
+		this.mealDetailId = mealDetailId;
+		this.food = food;
+		this.meal = meal;
 	}
 
-	// Getters and Setters
-    public int getMealDetailId() {
-        return mealDetailId;
-    }
+	public MealDetails(Food food, Meals meal) {
+		this.food = food;
+		this.meal = meal;
+	}
 
-    public void setMealDetailId(int mealDetailId) {
-        this.mealDetailId = mealDetailId;
-    }
+	public MealDetails(int mealDetailId) {
+		super();
+		this.mealDetailId = mealDetailId;
+	}
 
-    public int getFoodId() {
-        return foodId;
-    }
+	public int getMealDetailId() {
+		return mealDetailId;
+	}
 
-    public void setFoodId(int foodId) {
-        this.foodId = foodId;
-    }
+	public void setMealDetailId(int mealDetailId) {
+		this.mealDetailId = mealDetailId;
+	}
 
-    public int getMealId() {
-        return mealId;
-    }
+	public Food getFood() {
+		return food;
+	}
 
-    public void setMealId(int mealId) {
-        this.mealId = mealId;
-    }
+	public void setFood(Food food) {
+		this.food = food;
+	}
+
+	public Meals getMeal() {
+		return meal;
+	}
+
+	public void setMeal(Meals meal) {
+		this.meal = meal;
+	}
 }
