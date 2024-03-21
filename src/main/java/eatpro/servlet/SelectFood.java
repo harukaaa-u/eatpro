a3b0372e004f4a49d708bda8d501e835440411df
vanessacci <127009094+vanessacci@users.noreply.github.com>
2024-03-21 00:25:47 -0700

    debug select food

--- src/main/java/eatpro/servlet/SelectFood.java
+++ src/main/java/eatpro/servlet/SelectFood.java
@@ -29,52 +29,60 @@ public class SelectFood extends HttpServlet {
 		mealDetailsDao = MealDetailsDao.getInstance();
 	}
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 		Map<String, String> messages = new HashMap<>();
         req.setAttribute("messages", messages);
-        String breakfastId = String.valueOf(req.getAttribute("breakfast"));
-        String lunchId = String.valueOf(req.getAttribute("lunch"));
-        String dinnerId = String.valueOf(req.getAttribute("dinner"));
-        String snackId = String.valueOf(req.getAttribute("snack"));
+		String breakfastId = (String) req.getAttribute("breakfast");
+		String lunchId = (String) req.getAttribute("lunch");
+		String dinnerId = (String) req.getAttribute("dinner");
+		String snackId = (String) req.getAttribute("snack");
 		HashMap<String, Double> breakfastNutrients = (HashMap<String, Double>) req.getAttribute("breakfastNutrients");
 		HashMap<String, Double> lunchNurtients = (HashMap<String, Double>) req.getAttribute("lunchNurtients");
 		HashMap<String, Double> dinnerNutrients = (HashMap<String, Double>) req.getAttribute("dinnerNutrients");
 		HashMap<String, Double> snackNutrients = (HashMap<String, Double>) req.getAttribute("snackNutrients");
 
-		selectFood(Integer.parseInt(breakfastId), breakfastNutrients);
-		selectFood(Integer.parseInt(lunchId), lunchNurtients);
-		selectFood(Integer.parseInt(dinnerId), dinnerNutrients);
-		selectFood(Integer.parseInt(snackId), snackNutrients);
+		selectFood(breakfastId, breakfastNutrients, messages);
+			selectFood(lunchId, lunchNurtients, messages);
+			selectFood(dinnerId, dinnerNutrients, messages);
+			selectFood(snackId, snackNutrients, messages);
 
-		req.setAttribute("breakfastId", breakfastId);
-		req.setAttribute("lunchId", lunchId);
-		req.setAttribute("dinnerId", dinnerId);
-		req.setAttribute("snackId", snackId);
-
-		req.getRequestDispatcher("/displaymeal").forward(req, resp);
+			req.setAttribute("breakfastId", breakfastId);
+			req.setAttribute("lunchId", lunchId);
+			req.setAttribute("dinnerId", dinnerId);
+			req.setAttribute("snackId", snackId);
+			req.getRequestDispatcher("/displaymeal").forward(req, resp);
 	}
 
-	private void selectFood(int mealId, HashMap<String, Double> nutrients) {
-		if (mealId != null && !mealId.trim().isEmpty() && nutrients != null) {
-			Meals meal = foodDao.getMealById(mealId);
-			Food proteinFood = foodDao.getRandomFoodByMealFoodCategory(meal.getMealType(), "protein", nutrients.get("calories"));
-			Food carbohydrateFood = foodDao.getRandomFoodByMealFoodCategory(meal.getMealType(), "carbohydrates", nutrients.get("calories"));
-			Food vegetableFood = foodDao.getRandomFoodByMealFoodCategory(meal.getMealType(), "vegetables", nutrients.get("calories"));
-			MealDetails mealDetails1 = new MealDetails(meal, proteinFood);
-			mealDetailsDao.create(mealDetails1);
-			MealDetails mealDetails2 = new MealDetails(meal, carbohydrateFood);
-			mealDetailsDao.create(mealDetails2);
-			MealDetails mealDetails3 = new MealDetails(meal, vegetableFood);
-			mealDetailsDao.create(mealDetails3);
-		} else {
-			messages.put("title", "Invalid Meal.");
-		}
+	private void selectFood(String mealId, HashMap<String, Double> nutrients, Map<String, String> messages ) throws IOException {
+		try {
+					
+			if (mealId != null && !mealId.trim().isEmpty() && nutrients != null) {
+				Meals meal = mealsDao.getMealById(Integer.parseInt(mealId));
+				Food proteinFood = foodDao.getRandomFoodByMealFoodCategory(meal.getMealType(), "protein", nutrients.get("calories"));
+				Food carbohydrateFood = foodDao.getRandomFoodByMealFoodCategory(meal.getMealType(), "carbohydrates", nutrients.get("calories"));
+				Food vegetableFood = foodDao.getRandomFoodByMealFoodCategory(meal.getMealType(), "vegetables", nutrients.get("calories"));
+				MealDetails mealDetails1 = new MealDetails(proteinFood, meal);
+				mealDetailsDao.create(mealDetails1);
+				MealDetails mealDetails2 = new MealDetails(carbohydrateFood, meal);
+				mealDetailsDao.create(mealDetails2);
+				MealDetails mealDetails3 = new MealDetails(vegetableFood, meal);
+				mealDetailsDao.create(mealDetails3);
+			} else {
+				messages.put("title", "Invalid Meal.");
+			}
+
+        } catch (SQLException e) {
+			e.printStackTrace();
+			throw new IOException(e);
+        }
+		
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 	}
 
 
-	
+
 }
